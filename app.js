cd24e1de14de45e743e48173cf234da833dc7e3e
Diana Vlasenkova <dianadomino24work@gmail.com>
2020-12-07 11:40:43 +0300

    initial folders and package add

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const { PORT = 3000 } = process.env;
+
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
